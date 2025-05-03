3dc3226a0633380eb9f0d54ea42d3472a47f7787
Reef <148873332+Tinyliilfairy@users.noreply.github.com>
2025-05-03 22:23:02 +0300

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// Sign-up
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User registered" });
+  } catch (err) {
+    res.status(400).json({ message: "User registration failed", error: err.message });
+  }
+});
+
+// login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(401).json({ message: "Wrong password" });
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  res.json({ token });
+});
+
+// logout
+router.post("/logout", (req, res) => {
+  res.json({ message: "User logged out" });
+});
+
+module.exports = router;
